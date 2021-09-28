05ed719b08e0011280e13351eb90d688b5b523ab
SiwaniV <6003716@mborijnland.nl>
2021-09-28 10:35:00 +0200

    kleine style aanpassingen

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/blog')]
+
 class BlogController extends AbstractController
 {
     #[Route('/', name: 'blog_index', methods: ['GET'])]
