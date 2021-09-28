2f6e9c8de0dcfa2c621dc9ebae288e5a91179a32
SiwaniV <6003716@mborijnland.nl>
2021-09-28 11:25:06 +0200

    kleine aanpassingen in login systeem

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -44,14 +44,16 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         );
     }
 
+    /**
+     * @throws \Exception
+     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('default'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
